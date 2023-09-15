77f9c95706d0d524aad7597ed960f9ae64515dfa
dimansidorov <dimkasidorow@gmail.com>
2023-09-15 16:10:00 +0200

    created config.py, .env, .env.template

---
+++ cv_project/config.py
@@ -0,0 +1,11 @@
+import os
+
+'''SECRET KEY'''
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+'''DATABASE'''
+DB_NAME = os.environ.get('DB_NAME')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
